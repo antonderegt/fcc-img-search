bf7e8c974e7a783a445f2efb50de8e0d1d3b72fd
Anton de Regt <antonderegt@gmail.com>
2017-04-01 19:47:37 +0000

    Added new functionality

--- api.js
+++ api.js
@@ -1,51 +1,69 @@
 'use strict';
 
 module.exports = (app, db) => {
+const request = require('request')
+const CX = '002962581167205362641:hbbgdalzyts'
+const API_KEY = 'AIzaSyCVob1WbUVN8iElaZ0PIbBCSPliegGAtDw'
 
+const getApiData = (req, res, query, offset) => {
+  request.get('https://www.googleapis.com/customsearch/v1?key=' + API_KEY + '&cx=' + CX + '&searchType=image&q=' + query +'&start=' + offset, 
+  (err, response, body) => {
+    if (!err && response.statusCode == 200) {
+        let dataArr = []
+        let dataSet = JSON.parse(body).items
+        for(let i = 0; i < dataSet.length; i++) {
+            let data = dataSet[i]
+            let imgUrl = data.link
+            let altText = data.snippet
+            let pageUrl = data.contextLink
+            
+            let obj = {
+                "url": imgUrl,
+                "alt": altText,
+                "context": pageUrl
+            }
+            
+            dataArr.push(obj)
+        }
+        
+        res.send(dataArr)
+    }
+  })
+}
 
-const GoogleImages = require('google-images');
+const save = searchQuery => {
+    let collection = db.collection('queries')
+    collection.save(searchQuery, (err) => {
+        if(err) throw err
+    })
+}
 
-const client = new GoogleImages('002962581167205362641:hbbgdalzyts', 'AIzaSyDEmM3x95_WiwPS64iWY7dU7Cp5Q5cVMT8');
-
-client.search('Steve Angello')
-	.then(images => {
-	    console.log('image: ', images)
-// 		[{
-// 			"url": "http://steveangello.com/boss.jpg",
-// 			"type": "image/jpeg",
-// 			"width": 1024,
-// 			"height": 768,
-// 			"size": 102451,
-// 			"thumbnail": {
-// 				"url": "http://steveangello.com/thumbnail.jpg",
-// 				"width": 512,
-// 				"height": 512
-// 			}
-// 		}]
-// 		 */
-	});
-
-// paginate results
-client.search('Steve Angello', {page: 2});
-
-// search for certain size
-client.search('Steve Angello', {size: 'large'});
-
-
-
-
-    app.get('/api/imagesearch/*', (req, res) => {
-        let query = req.params[0]
-        let offset = req.query.offset
-        client.search('Steve Angello').then(data => {
-            res.send(data)
-        })
-        // res.send('Query: ' + query + ' Offset: ' + offset)
+    app.get('/api/imagesearch/:query(*)', (req, res) => {
+        let query = req.params.query
+        let offset = 1
+        if(req.query.offset) {
+            offset = req.query.offset
+        }
+        
+        let date = new Date().toISOString()
+        
+        let searchQuery = {
+            "query": query,
+            "date": date
+        }
+        
+        save(searchQuery)
+        
+        getApiData(req, res, query, offset)
     })
     
-    app.get('/api/latest/imagesearch/', (req, res) => {
-        let query = req.params[0]
-        let offset = req.query.offset
-        res.send('Query: ' + query + ' Offset: ' + offset)
+    app.get('/api/latest/imagesearch', (req, res) => {
+        let collection = db.collection('queries')
+        collection.find({}, {
+            "_id": 0
+        }).limit(10).toArray((err, data) => {
+            if(err) throw err
+            res.send(JSON.stringify(data))
+        })
     })
 }
